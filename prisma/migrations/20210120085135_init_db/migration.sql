0fb6f8a512bce68dbc9f310bdf430ffda0ad3db6
Jonatan Michiels <michielsjonatan@gmail.com>
2021-01-20 09:52:21 +0100

    Update prisma db config

--- prisma/migrations/20210120085135_init_db/migration.sql
+++ prisma/migrations/20210120085135_init_db/migration.sql
@@ -13,13 +13,13 @@ CREATE TABLE "Client" (
 -- CreateTable
 CREATE TABLE "Address" (
     "id" TEXT NOT NULL PRIMARY KEY,
-    "line" TEXT NOT NULL,
+    "addressLine" TEXT NOT NULL,
     "zip" TEXT NOT NULL,
     "city" TEXT NOT NULL,
     "state" TEXT,
     "country" TEXT,
-    "isBilling" BOOLEAN,
-    "isShipping" BOOLEAN,
+    "isBillingAddress" BOOLEAN,
+    "isShippingAddress" BOOLEAN,
     "clientId" TEXT,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL,
@@ -32,36 +32,26 @@ CREATE TABLE "Product" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "name" TEXT NOT NULL,
     "code" TEXT NOT NULL,
-    "price" INTEGER NOT NULL,
-    "description" TEXT,
+    "description" TEXT NOT NULL,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL,
-    "deletedAt" DATETIME
+    "deletedAt" DATETIME,
+    "productId" TEXT,
+    FOREIGN KEY ("productId") REFERENCES "Product" ("id") ON DELETE SET NULL ON UPDATE CASCADE
 );
 
 -- CreateTable
 CREATE TABLE "Order" (
     "id" TEXT NOT NULL PRIMARY KEY,
+    "quantity" INTEGER NOT NULL,
     "clientId" TEXT NOT NULL,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL,
     "deletedAt" DATETIME,
-    FOREIGN KEY ("clientId") REFERENCES "Client" ("id") ON DELETE CASCADE ON UPDATE CASCADE
-);
-
--- CreateTable
-CREATE TABLE "_OrderToProduct" (
-    "A" TEXT NOT NULL,
-    "B" TEXT NOT NULL,
-    FOREIGN KEY ("A") REFERENCES "Order" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY ("B") REFERENCES "Product" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+    "orderId" TEXT,
+    FOREIGN KEY ("clientId") REFERENCES "Client" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY ("orderId") REFERENCES "Order" ("id") ON DELETE SET NULL ON UPDATE CASCADE
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Client.email_unique" ON "Client"("email");
-
--- CreateIndex
-CREATE UNIQUE INDEX "_OrderToProduct_AB_unique" ON "_OrderToProduct"("A", "B");
-
--- CreateIndex
-CREATE INDEX "_OrderToProduct_B_index" ON "_OrderToProduct"("B");
