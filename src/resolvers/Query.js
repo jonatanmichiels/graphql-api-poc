efa7b2dd43714f9b1707b400e7e57296167869e2
Jonatan Michiels <michielsjonatan@gmail.com>
2021-01-20 13:39:30 +0100

    Bugfixes in graphql queries

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,4 @@
 const info = () => 'GraphQL API proof of concept';
-
 const clients = async (root, args, context) => context.prisma.client.findMany();
 const orders = async (root, args, context) => context.prisma.order.findMany();
 const products = async (root, args, context) =>
